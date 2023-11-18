4f83f14303937809be2b1c0d65bdedf8a4bb7abb
khanice <you@example.com>
2023-11-18 16:39:49 +0530

    error changes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import About from "../Components/About";
 import Footer from "../Components/Footer";
 import MyNavbar from "../Components/Navbar";
-import Info from "../Components/Info"; // Make sure the component name matches the import statement
+ // Make sure the component name matches the import statement
+ import Info from "../Components/info";
 import Services from "../Components/Services";
 import Expertise from "../Components/Expertise";
 import Contact from "../Components/Contact";
@@ -11,7 +12,7 @@ function HomePage() {
     <div>
       <MyNavbar />
       <About className="about" id="home"/>
-      <Info className="info" id="about"/>
+      <Info/>
       <Services className="services" id="service"/>
       <Expertise className="expertise"id="expertise" />
       <Contact className="contact"id="contacts" />
